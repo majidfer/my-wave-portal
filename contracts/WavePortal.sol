68095c71262563a662f0253aa8b770e4e8e6e372
feri <ferialfahri@gmail.com>
2022-11-23 13:46:16 +0000

    Initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("This is my wave portal smart contract!");
+    }
+}
